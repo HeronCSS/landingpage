2c656adf23444f5a30b99e48527227b708ead96c
HeronCSS <88357706+HeronCSS@users.noreply.github.com>
2024-03-05 15:36:11 -0300

    html, body, main, navbar

--- src/main/java/com/landingpage/landingpage/controllers/LandingPageController.java
+++ src/main/java/com/landingpage/landingpage/controllers/LandingPageController.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.landingpage.landingpage.models.Email;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
--- src/main/java/com/landingpage/landingpage/models/Email.java
+++ src/main/java/com/landingpage/landingpage/models/Email.java
@@ -7,4 +7,22 @@ public class Email {
 
     private String mensagem;
 
+
+    public String getNomeRemetente() {
+        return this.nomeRemetente;
+    }
+
+    public void setNomeRemetente(String nomeRemetente) {
+        this.nomeRemetente = nomeRemetente;
+    }
+
+    public String getMensagem() {
+        return this.mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+
 }
